4ed654c340df04c75e83f8199ed68776a8a85a2d
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-06-27 09:16:56 +0200

    minor

--- src/External/WixApi/Constants/WixConstants.cs
+++ src/External/WixApi/Constants/WixConstants.cs
@@ -5,7 +5,6 @@
         public const string BaseUrl = "https://www.wixapis.com";
         public const string AccountId = "website3583";
         public const string SiteId = "c069009d-e1e7-40c2-82aa-c906e0cdad8c";
-        public const string ApiKey = "IST.eyJraWQiOiJQb3pIX2FDMiIsImFsZyI6IlJTMjU2In0.eyJkYXRhIjoie1wiaWRcIjpcImQzNGZhZWYzLTE2OGQtNDViNC05ZmU2LTE5ZGU3ZWU1MjZiZlwiLFwiaWRlbnRpdHlcIjp7XCJ0eXBlXCI6XCJhcHBsaWNhdGlvblwiLFwiaWRcIjpcImU2YjRjNmNhLTkxNmEtNDMwMC04YWYwLWQ5N2I2MWI5NjIxZlwifSxcInRlbmFudFwiOntcInR5cGVcIjpcImFjY291bnRcIixcImlkXCI6XCI1ZGVmZGRjMy05ZTYzLTQxNTQtYWIxYS05MzY0NzYxYmRkODhcIn19IiwiaWF0IjoxNjczMDg5NTA0fQ.kNCNAr-AtMGhUt-h1KDiygmaOx3ixbHu68yBaKuOP5wYxryqkEs44_zTzGwi2Ge7dYo03YTMpEKI2rngR-Bvs4TPvswlp-2ETY6ctAaWx_FK1MXIMXN2FhqwHvyenCzMUYW4JxBCBq6ZUdmO8n2TnBNzzJvWPKYHyctgK2_QNjlb3Bgrq1LkbyjPAI6jzQpzMwkk7cfNhPndNwgxOC5d_HbfjtchQmqqKJvh9D9ER1BG8OB5Gojjm3Bk3uz8CtRSSHzdV-W4MTXJSr8uUuZWORjmqEJjXlBzxQgpPNuoYtneuKKQVF8pDZX6yF4LnWHyrijs9Hc-_wwTKlwKap1uaw";
-
+        public const string ApiKey = "xx";
     }
 }
--- src/External/WixApiOld/Constants/WixConstants.cs
+++ src/External/WixApiOld/Constants/WixConstants.cs
@@ -10,6 +10,6 @@ namespace WixApi.Constants
         public const string AccountId = "website3583";
         public const string SiteId = "c069009d-e1e7-40c2-82aa-c906e0cdad8c";
         public const string ApiKey =
-            "IST.eyJraWQiOiJQb3pIX2FDMiIsImFsZyI6IlJTMjU2In0.eyJkYXRhIjoie1wiaWRcIjpcIjMwNjNkNDMzLWI3ZTEtNDYyNy1iY2FlLTE0OGJlMTkyMjVlYlwiLFwiaWRlbnRpdHlcIjp7XCJ0eXBlXCI6XCJhcHBsaWNhdGlvblwiLFwiaWRcIjpcIjEwNTQzOGIyLWNlOTMtNDc1OS04YTI0LTZkMTBhNTIyNjU4OVwifSxcInRlbmFudFwiOntcInR5cGVcIjpcImFjY291bnRcIixcImlkXCI6XCI1ZGVmZGRjMy05ZTYzLTQxNTQtYWIxYS05MzY0NzYxYmRkODhcIn19IiwiaWF0IjoxNzUwNzc2MzI3fQ.lcJehMItV3nLSfiJSud-SwwQV3pWbnye1wiU9JuJd6gocm_oYU0d3Wa04MHDEg4vvqC6TYzRyM8Bs6n6BUlvbyGwK3asYdqwB1PzfbxfLJ3Vcj3NThS1TgRMDJR03_969-yd8mABRxgVCdDlmF-fpFoHS0V86OZTL0b1SO64kOwYVJnC-gRL7NL4Hhki0BeVThxky1KB5xGm5RxNVZDCJVmilNmLuFIsPEoltVBtwNCtvuJDF0l1J4bDt71RP2q8xt7zwe8Ed4odtxBSyZoOW19iIUFCXhORMSZZEUx3Ug_ibi-9OAAGGfAhi_9zVTG6sflFVC8F-xpgDVYME23u_Q";
+            "xxx";
     }
 }
