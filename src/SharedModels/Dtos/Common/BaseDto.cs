200e6d93267f820e5b60e1c6462e370f0addfce9
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-06-27 16:36:07 +0200

    update

---
+++ src/SharedModels/Dtos/Common/BaseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.Dtos.Common
+{
+    public record BaseDto
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/SharedModels/Dtos/Contacts/ContactDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedModels.Dtos.Common;
+
+namespace SharedModels.Dtos.Contacts
+{
+    public record ContactDto(string WixId, string Name, string Email, string Phone, string Branche)
+        : BaseDto;
+}
