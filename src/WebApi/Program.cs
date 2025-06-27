023aa17dd39ed6d4070e04b20719fb1e578f961a
Daniel Hufnagl <d.hufnagl@codelisk.com>
2025-06-27 14:39:37 +0200

    update

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,3 @@
-
-using Aspire.Hosting;
-
 namespace WebApi
 {
     public class Program
@@ -8,8 +5,6 @@ namespace WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            builder.AddServiceDefaults();
 
             // Add services to the container.
             builder.Services.AddControllers();
@@ -24,11 +19,9 @@ namespace WebApi
                 app.MapOpenApi();
             }
 
-            app.MapDefaultEndpoints();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
